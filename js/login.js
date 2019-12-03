35035a0bc52254d6c97a795cddabce27be330058
csabalaszlo <58145409+csabalaszlo@users.noreply.github.com>
2019-12-03 07:34:44 +0200

    home-page

--- js/login.js
+++ js/login.js
@@ -1,24 +1,50 @@
 
 $(document).ready(function(){
+
+$("#login-alert").hide();
+
 //alert("1");
 isCityHall = localStorage.getItem("isCityHall");
+console.log("isCityHall="+isCityHall);
 //alert("1");
 
+//redirect if logged in
+var user = localStorage.getItem("user");
+if(user !== null){
+  if(isCityHall === "true"){
+   window.location.href="home_hall.html";
+ }else{
+    window.location.href="home_android.html";
+ }
+}
+
+
  $("#login-button").click(function(){
+   $("#login-alert").hide();
    var email = $("#email").val();
    var password = $("#password").val();
 
-   if(isCityHall === true){
+   console.log("email="+email);
+   console.log("password="+password);
+   console.log("isCityHall = "+isCityHall);
+
+   if(isCityHall === "true"){
      if(email === "admin" && password==="1234"){
+       localStorage.setItem("user", email);
        window.location.href = 'home_hall.html';
      }else{
-       alert("bad password or email");
+       //server
+       $("#login-alert").html("Bad password or email");
+       $("#login-alert").show();
      }
   } else {
     if(email === "user" && password==="1234"){
+       localStorage.setItem("user", email);
       window.location.href = 'home_android.html';
     }else{
-      alert("bad password or email");
+      //server
+      $("#login-alert").html("Bad password or email");
+      $("#login-alert").show();
     }
   }
  });
