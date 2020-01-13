d8f7f44aeaef1c7ed373f6d5450bfed6b71a5c82
csabalaszlo <58145409+csabalaszlo@users.noreply.github.com>
2020-01-13 21:15:28 +0200

    loading bar

--- js/problems.js
+++ js/problems.js
@@ -1,5 +1,25 @@
 var incrId = 12;
 
+function getModal(modalName, title, bodyId){
+  return `
+  <!-- Modal -->
+<div class="modal fade" id="` + modalName + `" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">`+ title +`</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div id="` + bodyId + `" class="modal-body">
+
+    </div>
+  </div>
+</div>
+</div>`
+}
+
 $(document).ready(function(){
 
 
@@ -24,6 +44,7 @@ $(document).ready(function(){
     var location = "";
     var priority = "";
     var upvotes = "";
+    var completed = "40";
 
     if(selected === "1"){
       photo = "https://www.freeiconspng.com/uploads/lion-icon-6.png";
@@ -31,23 +52,37 @@ $(document).ready(function(){
       location = "Observator";
       priority = "HIGH";
       upvotes = "61";
+      completed = "40";
     }else if(selected === "2"){
       photo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiG98-3YswD7lA2a9OeXeAmwcrvdVV7ixmBJKJZcUGKoO4ANJcEQ&s";
       description = "Everybody can help";
       location = "City Centre";
       priority = "HIGH";
         upvotes = "63";
+        completed = "20";
     }else if(selected === "13"){
       photo = "./images/city.png";
       description = "description";
       location = "Gruia";
       priority = "HIGH";
       upvotes = "0";
+      completed = "60";
     }
 
     if(photo.length > 0){
         $("#more-body").html(`
-          <form>
+          <div class="progress">
+         <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+           `+ completed +`% Completed
+         </div>
+         <div id="user-comments-pop"></div>
+          <div id="hall-comments-pop"></div>
+       </div>
+          <form style="margin-top:20px">
+          <div class="form-group text-center">
+             <button id="user-comments" type="button" class="btn btn-info">User Comments</button>
+             <button id="hall-comments" type="button" class="btn btn-info">City Hall Comments</button>
+           </div>
            <div class="form-group">
             <label for="problem-description2">Description</label>
             <textarea disabled class="form-control" id="problem-description2" placeholder="` + description + `" rows="2"/>
@@ -73,7 +108,12 @@ $(document).ready(function(){
          `);
      }else{
        $("#more-body").html(`
-         <form>
+         <div class="progress">
+        <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:40%">
+          40% Completed
+          </div>
+        </div>
+         <form style="margin-top:20px">
          <div class="form-group">
           <label for="problem-description2">Description</label>
           <textarea disabled class="form-control" id="problem-description2" placeholder="` + description + `" rows="2"/>
@@ -93,7 +133,30 @@ $(document).ready(function(){
          </form>
         `);
      }
+
+     $("#user-comments").click(function(){
+       
+       $("#user-comments-pop").html(getModal("user-comments-pop-modal","User Comments", "user-comments-pop-modal-body"));
+       $("#user-comments-pop-modal").modal();
+
+       $("#user-comments-pop-modal-body").html(`
+         <div  style="overflow-y: scroll;">
+         <table id="example" class="table table-striped table-bordered">
+                 <tbody>
+                   <button id="place-1" type="button" class="btn btn-info btn-block place">
+                       Pizzeria Seba
+                   </button>
+                   <button id="place-2" type="button" class="btn btn-info btn-block place">
+                       Stradale
+                   </button>
+                 </tbody
+             </table>
+             </div>
+         `);
+     });
+
   });
+
 
   $("#submit-problem-button").click(function(){
     //send data to server...
