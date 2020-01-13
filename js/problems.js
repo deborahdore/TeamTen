86bcb1b3ce30885143db8639103a7caea0f9dada
csabalaszlo <58145409+csabalaszlo@users.noreply.github.com>
2020-01-13 21:44:06 +0200

    add comments pop up

--- js/problems.js
+++ js/problems.js
@@ -156,19 +156,89 @@ $(document).ready(function(){
        $("#user-comments-pop-modal").modal();
 
        $("#user-comments-pop-modal-body").html(`
-         <div  style="overflow-y: scroll;">
-         <table id="example" class="table table-striped table-bordered">
-                 <tbody>
-                   <button id="place-1" type="button" class="btn btn-info btn-block place">
-                       Pizzeria Seba
-                   </button>
-                   <button id="place-2" type="button" class="btn btn-info btn-block place">
-                       Stradale
-                   </button>
-                 </tbody
-             </table>
+         <div class="row bootstrap snippets">
+     <div class="col-md-6 col-md-offset-2 col-sm-12">
+         <div class="comment-wrapper">
+             <div class="panel panel-info">
+
+                 <div class="panel-body">
+                     <textarea id="comment-area" class="form-control" placeholder="write a comment..." rows="3"></textarea>
+                     <br>
+                     <button type="button" id="post-comment" class="btn btn-info pull-right">Post</button>
+                     <div class="clearfix"></div>
+                     <hr>
+                     <ul class="media-list">
+                         <li class="media">
+                             <a href="#" class="pull-left">
+                                 <img src="https://bootdey.com/img/Content/user_2.jpg" alt="" class="img-circle">
+                             </a>
+                             <div class="media-body">
+                                 <span class="text-muted pull-right">
+                                     <small class="text-muted">30 min ago</small>
+                                 </span>
+                                 <strong class="text-success">@LaurenceCorreil</strong>
+                                 <p>
+                                    No, it isn't. Please do not go out!
+                                 </p>
+                             </div>
+                         </li>
+                         <li class="media">
+                             <a href="#" class="pull-left">
+                                 <img src="https://bootdey.com/img/Content/user_3.jpg" alt="" class="img-circle">
+                             </a>
+                             <div class="media-body">
+                                 <span class="text-muted pull-right">
+                                     <small class="text-muted">32 min ago</small>
+                                 </span>
+                                 <strong class="text-success">@JohnNida</strong>
+                                 <p>
+                                    I mean, can somebody comfirm this?
+                                 </p>
+                             </div>
+                         </li>
+                         <li class="media">
+                             <a href="#" class="pull-left">
+                                 <img src="https://bootdey.com/img/Content/user_3.jpg" alt="" class="img-circle">
+                             </a>
+                             <div class="media-body">
+                                 <span class="text-muted pull-right">
+                                     <small class="text-muted">32 min ago</small>
+                                 </span>
+                                 <strong class="text-success">@JohnNida</strong>
+                                 <p>
+                                     Isn't this a false alarm?
+                                 </p>
+                             </div>
+                         </li>
+                     </ul>
+                 </div>
              </div>
+         </div>
+
+     </div>
+ </div>
          `);
+
+
+   $("#post-comment").click(function(){
+     var message = $("#comment-area").val();
+      $(".media-list").prepend(`
+        <li class="media">
+            <a href="#" class="pull-left">
+                <img src="https://bootdey.com/img/Content/user_1.jpg" alt="" class="img-circle">
+            </a>
+            <div class="media-body">
+                <span class="text-muted pull-right">
+                    <small class="text-muted">0 min ago</small>
+                </span>
+                <strong class="text-success">@User</strong>
+                <p>
+                  `+message+` </a>.
+                </p>
+            </div>
+        </li>
+        `)
+       });
    });
 
      $(".upvote").click(function(){
