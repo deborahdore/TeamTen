e5d56134fc974a8f2173d9837a474711c060d2ae
Ghisa.Ro.Raul <raulghisa@yahoo.com>
2020-01-14 20:08:17 +0200

    added discounts page

---
+++ js/discounts.js
@@ -0,0 +1,26 @@
+let remainingDiamonds = 3;
+
+const initialize = () => {
+    $("#discounts-back-button").click(function () {
+        window.location.href = "home_android.html";
+    });
+
+    $("#remaining-diamonds").text(remainingDiamonds + " x");
+
+    $(".text-block-dacia").text("5% discount");
+
+    $(".discount").click(() => {
+        if (remainingDiamonds > 0) {
+            confirm("Are you sure you want to use the credit?");
+            $("#remaining-diamonds").text(--remainingDiamonds + " x");
+        } else {
+            alert("You don't have enough credits!");
+        }
+    })
+
+    $(".text-block-ctp").text("1 free ticket");
+    $(".text-block-allview").text("5% discount");
+    $(".text-block-cfr").text("20% discount");
+}
+
+$(document).ready(initialize);
--- js/home.js
+++ js/home.js
@@ -1,30 +1,34 @@
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $("#logout-button").click(function(){
+  $("#logout-button").click(function () {
     localStorage.removeItem("user");
-    window.location.href="index.html";
-   });
+    window.location.href = "index.html";
+  });
 
-   isCityHall = localStorage.getItem("isCityHall");
+  isCityHall = localStorage.getItem("isCityHall");
 
-   $("#city-problems-button").click(function(){
-     if(isCityHall === "true"){
-      window.location.href="problems_hall.html";
-     }else{
-       window.location.href="problems_android.html";
-     }
-    });
+  $("#city-problems-button").click(function () {
+    if (isCityHall === "true") {
+      window.location.href = "problems_hall.html";
+    } else {
+      window.location.href = "problems_android.html";
+    }
+  });
 
-   $("#trash-report").click(() => {
-    window.location.href="trash_report.html";
-   })
+  $("#trash-report").click(() => {
+    window.location.href = "trash_report.html";
+  })
 
-   $("#traffic-info").click(function(){
-     window.location.href="traffic_info.html";
-   })
-   $("#register-requests-button").click(() => {
-     window.location.href="register_volunteer.html";
-   })
+  $("#traffic-info").click(function () {
+    window.location.href = "traffic_info.html";
+  })
+  $("#register-requests-button").click(() => {
+    window.location.href = "register_volunteer.html";
+  })
+
+  $("#discounts-button").click(() => {
+    window.location.href = "discounts.html";
+  })
 })
